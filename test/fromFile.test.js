47455404b93e2270c18e61ca87cbe36388fc99a2
tpluscode <tpluscode@users.noreply.github.com>
2022-10-02 21:48:07 +0200

    feat: support trig out of the box

--- test/fromFile.test.js
+++ test/fromFile.test.js
@@ -19,7 +19,7 @@ describe('fromFile', () => {
     const stream = fromFile(resolve(__dirname, 'support/example.ttl'), { baseIRI: 'http://example.org/' })
     const dataset = await rdf.dataset().import(stream)
 
-    strictEqual(dataset.toCanonical(), example().toCanonical())
+    strictEqual(dataset.toCanonical(), example.defaultGraph().toCanonical())
   })
 
   it('should combine extensions with default', async () => {
@@ -30,8 +30,24 @@ describe('fromFile', () => {
     const stream = fromFile(resolve(__dirname, 'support/example.nt'), { extensions })
     const dataset = await rdf.dataset().import(stream)
 
-    strictEqual(dataset.toCanonical(), example().toCanonical())
+    strictEqual(dataset.toCanonical(), example.defaultGraph().toCanonical())
   })
+
+  const commonExtensions = [
+    ['json', example.defaultGraph],
+    ['jsonld', example.namedGraph],
+    ['n3', example.defaultGraph],
+    ['trig', example.namedGraph],
+    ['nq', example.namedGraph]
+  ]
+  for (const [extension, expected] of commonExtensions) {
+    it(`should load ${extension} out of the box`, async () => {
+      const stream = fromFile(resolve(__dirname, `support/example.${extension}`))
+      const dataset = await rdf.dataset().import(stream)
+
+      strictEqual(dataset.toCanonical(), expected().toCanonical())
+    })
+  }
 
   it('should throw an error if the file extension is unknown', () => {
     throws(() => {
